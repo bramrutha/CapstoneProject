45dc9c9dafd03ef45e6186322099fe7487793cc3
bramrutha <37473261+bramrutha@users.noreply.github.com>
2020-11-07 18:26:20 -0600

    Update immigration_utility.py
    Updated comments

--- src/immigration_utility.py
+++ src/immigration_utility.py
@@ -2,8 +2,10 @@ from pyspark.sql.types import IntegerType,DateType
 from pyspark.sql.functions import udf
 from datetime import datetime,timedelta
 
+#udf to convert SAS date to string date format
 sas_to_date_udf = udf(lambda x: x if x is None else (timedelta(days=x) + datetime(1960, 1, 1)).strftime("%Y-%m-%d"))
 
+#function to convert the column type
 def cast_type(df,cols):
     for k,v in cols.items():
         df = df.withColumn(k,df[k].cast(v))
