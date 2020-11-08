3c03cafa155f59ccce1ede8f8c98158a3f1365f0
bramrutha <37473261+bramrutha@users.noreply.github.com>
2020-11-07 18:25:02 -0600

    Update immigration_transform.py
    Updated comments

--- src/immigration_transform.py
+++ src/immigration_transform.py
@@ -1,10 +1,9 @@
 import configparser
-import logging
 from pyspark.sql.types import IntegerType
 import immigration_utility as fn
 from pyspark.sql import functions as F
 
-
+#Read config file 
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
 immigration_save_path = config['SAVE']['IMMIGRATION']
@@ -15,6 +14,10 @@ airport_save_path = config['SAVE']['AIRPORT']
 
 
 class ImmTransform:
+    """
+    This class performs transformation operations on the dataset and
+    saves the results in parquet format in S3      
+    """
 
     def __init__(self,spark):
         self.spark = spark
@@ -23,16 +26,18 @@ class ImmTransform:
 
 
     def transform_i94_data(self):
-        logging.debug("Inside I94 transformation")
-        #i94_df = self.spark.read.csv(self.load_path + config['LOAD']['I94_DATA'], header=True, mode='PERMISSIVE', inferSchema=True)
+        #Read the Immigration dataset in SAS format 
         i94_df = self.spark.read.format('com.github.saurfang.sas.spark').load(self.load_path + config['LOAD']['I94_DATA'] )
+        
         #drop the columns that are not required for analysis
         i94_drop_cols =  ['count','visapost','occup','matflag','biryear','insnum']
         i94_df = i94_df.drop(*i94_drop_cols)
-
+        
+        #convert the numeric columns to Integer datatype
         int_cols = ['cicid', 'i94yr', 'i94mon', 'i94cit', 'i94res', 'arrdate', 'i94mode','depdate', 'i94bir', 'i94visa']
         i94_df = fn.cast_type(i94_df,dict(zip(int_cols,len(int_cols)*[IntegerType()])))
-
+        
+        #convert the SAS date format to String format
         date_cols = ['arrdate','depdate']
         for c in date_cols:
             i94_df = i94_df.withColumn(c,fn.sas_to_date_udf(i94_df[c]))
@@ -47,16 +52,23 @@ class ImmTransform:
                          .withColumn('day',F.dayofmonth(date_df.date))  \
                          .withColumn('dayofweek',F.dayofweek(date_df.date))  \
                          .withColumn('dayofyear',F.dayofyear(date_df.date))
-
+        
+        #Write the dates dataframe to a file in S3 in parquet format
         date_df.write.mode('overwrite').parquet(self.save_path + date_save_path)
 
+        #Write the I94 dataframe to a file in S3 in parquet format partioned by year and month   
         i94_df.write.partitionBy('i94yr','i94mon').mode('overwrite').parquet(self.save_path + immigration_save_path)
 
 
     def transform_weather_data(self):
-
+        
+        #read the weather dataset
         weather_df = self.spark.read.csv(self.load_path + config['LOAD']['WEATHER_DATA'], header=True, mode='PERMISSIVE', inferSchema=True)
+        
+        #filter the dataset to fetch records with year '2013'
         weather_df = weather_df.filter(weather_df.dt.startswith('2013'))
+        
+        #aggregate the columns
         weather_df = weather_df.groupBy('Country').agg({'AverageTemperature' : 'avg', 'Latitude' : 'first', 'Longitude' : 'first'})
         weather_df = weather_df.withColumnRenamed('avg(AverageTemperature)','Temperature') \
                                .withColumnRenamed('first(Latitude)', 'Latitude')            \
@@ -64,11 +76,12 @@ class ImmTransform:
 
         weather_df = weather_df.withColumn('Country', F.upper(weather_df.Country))
 
+        #replace the names in Country column    
         countries = [("Country", "Congo (Democratic Republic Of The)", "Congo"), ("Country", "Côte D'Ivoire", "Ivory Coast")]
         for field,old,new in countries:
             weather_df = weather_df.withColumn(field,F.when(weather_df[field] == old,new).otherwise(weather_df[field]))
 
-
+        #Read the look-up file containg the Country code and the Country name
         i94ctry_df = self.spark.read.csv(self.load_path + config['LOOKUP']['I94CTRY'], header=True,mode='PERMISSIVE',inferSchema=True)
 
         countries  = [("I94CTRY", "BOSNIA-HERZEGOVINA", "BOSNIA AND HERZEGOVINA"),
@@ -86,21 +99,30 @@ class ImmTransform:
         country_df = country_df.drop('Country')
         country_df = country_df.withColumnRenamed('I94CTRY','Country')   \
                                .withColumnRenamed('Code','Country_code')
-
+        
+        #Write the weather dataframe in S3 in parquet format
         country_df.write.save(self.save_path + weather_save_path, mode="overwrite", format="parquet", partitionBy=None)
 
 
 
     def transform_demographics_data(self):
+        
+        #Read the demographics dataset
         demo_df = self.spark.read.csv(self.load_path + config['LOAD']['DEMO_DATA'], sep=';',header=True,mode='PERMISSIVE', inferSchema=True)
+        
+        #perform aggregations
         state_agg_df = demo_df.groupBy('State Code').agg({'State' : 'First','Median Age' : 'avg','Male Population':'sum','Female Population' : 'sum',
                                                       'Total Population' : 'sum'})
+        
+        #rename column names
         state_agg_df = state_agg_df.withColumnRenamed('State Code','State_code')  \
                                      .withColumnRenamed('first(State)','State')     \
                                      .withColumnRenamed('sum(Male Population)','Male_population') \
                                      .withColumnRenamed('sum(Female Population)', 'Female_population') \
                                      .withColumnRenamed('avg(Median Age)', 'Median_age') \
                                      .withColumnRenamed('sum(Total Population)', 'Total_population')
+        
+        #create a dataframe containing column values as columns
         race_df = demo_df.groupBy('State Code').pivot('Race').sum('Count')
         race_df = race_df.withColumnRenamed('State Code','State_code')  \
                          .withColumnRenamed('American Indian and Alaska Native','AmericanIndian_and_AlaskaNative')     \
@@ -110,12 +132,17 @@ class ImmTransform:
         state_df = state_agg_df.join(race_df,state_agg_df.State_code == race_df.State_code)
         state_df = state_df.drop('State_Code')
 
+        #write the state dataframe to S3 in parquet format
         state_df.write.save(self.save_path + state_save_path, mode="overwrite", format="parquet", partitionBy=None)
 
 
     def transform_airport_data(self):
+        
+        #read airports dataset
         airport_df = self.spark.read.csv(self.load_path + config['LOAD']['AIRPORT_DATA'], header=True,mode='PERMISSIVE', inferSchema=True)
         airport_df = airport_df.filter((airport_df.iata_code.isNotNull()) & (airport_df.iso_country == 'US')).select('ident','type','name','iso_country','iata_code')
+        
+        #write the airport dataframe to S3 in parquet format
         airport_df.write.save(self.save_path + airport_save_path, mode="overwrite", format="parquet", partitionBy=None)
 
 
